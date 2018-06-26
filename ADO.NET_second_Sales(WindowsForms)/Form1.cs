d3510db9ba6a4b57867090c3b88fc13ad4d85430
Admin <Admin@X>
2018-06-27 02:00:39 +0300

    Fix Project

--- ADO.NET_second_Sales(WindowsForms)/Form1.cs
+++ ADO.NET_second_Sales(WindowsForms)/Form1.cs
@@ -15,17 +15,107 @@ namespace ADO.NET_second_Sales_WindowsForms_
     public partial class Form1 : Form
     {
         SqlConnection conn = null;
+        private static string strConnection;
+        List<string> listAllTables;
+        DataSet dataset;
+        SqlDataAdapter adapter;
         public Form1()
         {
             InitializeComponent();
+            listAllTables = new List<string>();
+            dataset = new DataSet();
+            adapter = new SqlDataAdapter();
             Load += Form1_Load;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            conn.ConnectionString = ConfigurationManager.ConnectionStrings["ConnSales"].ConnectionString;
+            conn = new SqlConnection();
+            strConnection = ConfigurationManager.ConnectionStrings["ConnSales"].ConnectionString;
+            conn.ConnectionString = strConnection;
+            SqlCommand command = new SqlCommand("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE LIKE '%TABLE%'", conn);
 
-            SqlCommand commandTables = new SqlCommand("select * from Sales.information_schema.tables where TABLE_TYPE like '%TABLE%'", conn);
+            using (SqlConnection connection = new SqlConnection())
+            {
+
+                try
+                {
+                    conn.Open();
+                    MessageBox.Show("Connection", "Start");
+                    adapter = new SqlDataAdapter(command);
+                    adapter.Fill(dataset);
+
+                    foreach (DataTable dt in dataset.Tables)
+                    {
+                        foreach (DataRow row in dt.Rows)
+                        {
+                            var cells = row.ItemArray;
+                            for (int i = 0; i < cells.Length; i++)
+                            {
+                                if(i == 2)
+                                {
+                                    listAllTables.Add(cells[i].ToString());
+                                    Console.WriteLine();
+                                }
+                            }
+                        }
+                    }
+
+                    comboBox1.Items.AddRange(listAllTables.ToArray());
+                }
+
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Error", $"{ex.Message}");
+                }
+
+                finally
+                {
+                    MessageBox.Show("Connection", "Close");
+                    conn.Close();
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlDataReader rdr = null;
+                DataTable dataTable = new DataTable();
+                string table = comboBox1.SelectedItem.ToString();
+                string sqlQeury = $"select * from {table}";
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sqlQeury, conn);
+                rdr = cmd.ExecuteReader();
+                int line = 0;
+                while (rdr.Read())
+                {
+                    if (line == 0)
+                    {
+                        for (int i = 0; i < rdr.FieldCount; i++)
+                        {
+                            dataTable.Columns.Add(rdr.GetName(i));
+                        }
+                    }
+                    line++;
+                    DataRow row = dataTable.NewRow();
+                    for (int i = 0; i < rdr.FieldCount; i++)
+                    {
+                        row[i] = rdr[i];
+                    }
+                    dataTable.Rows.Add(row);
+                }
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error", $"{ex.Message}");
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
